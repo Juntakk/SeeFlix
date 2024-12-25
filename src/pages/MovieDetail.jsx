07068ecc91807d5ff789a3f3a21ce0cb68689f48
Nicolas Gauthier <nick.habashi@gmail.com>
2024-12-25 12:12:26 -0500

    Refactors and style changes

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -31,13 +31,13 @@ export const MovieDetail = () => {
         <div className="max-w-sm">
           <img className="rounded" src={image} alt={movie.title} />
         </div>
-        <div className="max-w-2xl text-gray-700 text-lg dark:text-white">
-          <h1 className="text-4xl font-bold my-3 text-center lg:text-left">
+        <div className="max-w-2xl text-gray-800 text-lg dark:text-white">
+          <h1 className="text-4xl font-bold my-3 text-center lg:text-left italic">
             {movie.title}
           </h1>
           <p className="my-4">{movie.overview}</p>
           {movie.genres ? (
-            <p className="my-7 flex flex-wrap gap-2">
+            <p className="my-7 flex flex-wrap gap-1">
               {movie.genres.map((genre) => (
                 <span
                   className="mr-2 border border-gray-200 rounded dark:border-gray-600 p-2"
@@ -54,7 +54,7 @@ export const MovieDetail = () => {
           <div className="flex items-center">
             <svg
               aria-hidden="true"
-              className="w-5 h-5 text-yellow-400"
+              className="w-6 h-6 text-yellow-400"
               fill="currentColor"
               viewBox="0 0 20 20"
               xmlns="http://www.w3.org/2000/svg"
@@ -63,7 +63,7 @@ export const MovieDetail = () => {
               <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
             </svg>
             <p className="ml-2 text-gray-900 dark:text-white">
-              {movie.vote_average}
+              {movie.vote_average} / 10
             </p>
             <span className="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400"></span>
             <span className="text-gray-900 dark:text-white">
@@ -78,12 +78,12 @@ export const MovieDetail = () => {
 
           <p className="my-4">
             <span className="mr-2 font-bold">Budget:</span>
-            <span>{movie.budget}</span>
+            <span>{movie?.budget?.toLocaleString()} $</span>
           </p>
 
           <p className="my-4">
             <span className="mr-2 font-bold">Revenue:</span>
-            <span>{movie.revenue}</span>
+            <span>{movie?.revenue?.toLocaleString()} $</span>
           </p>
 
           <p className="my-4">
@@ -97,6 +97,7 @@ export const MovieDetail = () => {
               href={`https://www.imdb.com/title/${movie.imdb_id}`}
               target="_blank"
               rel="noreferrer"
+              className="underline"
             >
               {movie.imdb_id}
             </a>
